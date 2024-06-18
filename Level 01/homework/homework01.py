9f5da6d3fb23c343b35d7bf70009591cda42ee49
Anama2023 <anamatchkur@gmail.com>
2024-06-18 13:57:52 +0200

    commit

---
+++ Level 01/homework/homework01.py
@@ -0,0 +1,8 @@
+name = "Anamaria"
+surname = "kurkhashvili"
+age = 10
+height = 155
+wants_programing = "wants_programing"
+hobby = "drawing"
+
+print(name  + " " + surname + " " + str(age) + " " + str(height) + " " + wants_programing + " " + hobby)
