fb1550d192377d9066c76e7cc12d076587ccfe88
Anama2023 <anamatchkur@gmail.com>
2024-07-07 20:37:30 +0200

    commit

---
+++ Level 09/classwork/classwork.py
@@ -0,0 +1,21 @@
+
+Wins = int(input("Enter how many do you save:"))
+
+# 1 win = 3 points
+# 1 tie = 1 points
+# Calculate the score
+Wins = float(Wins)
+
+
+
+
+balance = str(input("Wins"))
+
+# Concatenate the 2 string to produce a masseg
+message = "amount in 1 year: " + balance
+
+
+
+
+
+print(message)
---
+++ Level 09/homework/3.homework/page1.py
@@ -0,0 +1,13 @@
+# 3) გააკეთეთ ეს პრაქტიკური სამუშაო
+
+ # input example   ·   Expected Outpud
+ # savings = 150   ·   Amount in 1 year: 157.5
+ # savings = 250   ·   Amount in 1 year:262.5
+
+
+user_input = int(input("Sheiyvanet tanxis odenoba: "))
+year_balance = user_input * 5 * 12
+print("erti wlis dagrovebuli tanxa sheadgens " + str(year_balance))
+
+
+
