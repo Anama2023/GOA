c078be84f23dd0c4c1088033594c80f7b4f7b21e
Anama2023 <anamatchkur@gmail.com>
2024-07-27 15:38:39 +0200

    commit

---
+++ Level 11/homework/1homework.py
@@ -0,0 +1,8 @@
+# დავალება: მომხმარებელს შემოატანინეთ ორი რიცხვი, და შემდეგ შედარებითი ოპერატორბით შეადარეთ, თუ რიცხვები უდრიან ერთმანეთს დაგვიპრინტოს True, ხოლო სხვა შემთხვევაში False.
+
+first_number = int(input("enter first number: "))
+second_number = int(input("enter second number: "))
+if first_number == second_number :
+    print(True)
+else: 
+    print(False)
