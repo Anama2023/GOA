7240a3e59106cba2c6cba5722f667df9cf93abc6
Anama2023 <anamatchkur@gmail.com>
2024-10-03 20:52:45 +0200

    commit

---
+++ Level 23/classwork/classwork.py
@@ -0,0 +1,26 @@
+# format
+
+#name = "anamaria"
+#last_name = "kurkhashvili"
+
+#rame = f"my name is",{name},"and my last name is",{last_name}
+#print(rame)
+
+# join
+
+#name = "anamaria"
+#last_name = "kurkhashvili"
+
+#rame = f"my name is",{0},"and my last name is",{1}
+#print(rame)
+
+
+# split
+
+
+
+
+
+# (4)
+
+x = str.('','')
